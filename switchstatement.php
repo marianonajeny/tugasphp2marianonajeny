98c9e5e7de9b5ad31d99ac018578f8529858bb57
marianonajeny <119488816+marianonajeny@users.noreply.github.com>
2023-03-06 14:00:57 -0800

    Add files via upload

---
+++ switchstatement.php
@@ -0,0 +1,25 @@
+<?php
+
+$harga = "A";
+
+switch ($harga) {
+    case 'A':
+    echo "Rp.200.000" . PHP_EOL;
+    break;
+
+    case 'B':
+    echo "Rp.350.000" . PHP_EOL;
+    break;
+
+    case 'C':
+    echo "Rp.600.000" . PHP_EOL;
+    break;
+
+    case 'D':
+    echo "Rp.650.000" . PHP_EOL;
+    break;
+
+    case 'E':
+    echo "Rp.700.000" . PHP_EOL;
+    break;
+}
---
+++ tenaryoperator.php
@@ -0,0 +1,5 @@
+<?php
+
+$bilangan = 58;
+
+echo $bilangan % 2 === 0 ? "bilangan genap" : "bilangan ganjil";
